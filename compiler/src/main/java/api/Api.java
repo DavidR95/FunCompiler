7048768c6c2b69888776303420078a6d32899e4e
David Robertson <dmichaelrobertson@gmail.com>
2017-10-22 15:53:40 +0100

    Add Gson dependency for Maven.

--- compiler/src/main/java/api/Api.java
+++ compiler/src/main/java/api/Api.java
@@ -2,11 +2,13 @@ package api;
 
 import static spark.Spark.*;
 import java.io.*;
+import com.google.gson.Gson;
 import fun.FunRun;
 
 public class Api {
 
     public static void main(String[] args) {
+        Gson gson = new Gson();
         get("/", (req, res) -> {
             return "Hello world!";
         });
