163e3f23ad4b5943defe591babf072b0ddb8c9ea
David Robertson <dmichaelrobertson@gmail.com>
2017-10-23 11:47:41 +0100

    Move index page logic from route to controller.

--- app/app/Http/Controllers/IndexController.php
+++ app/app/Http/Controllers/IndexController.php
@@ -6,5 +6,11 @@ use Illuminate\Http\Request;
 
 class IndexController extends Controller
 {
-    //
+    public function index()
+    {
+        $client = new \GuzzleHttp\Client();
+        $res = $client->request('POST', 'http://compiler:4567');
+        $body = json_decode($res->getBody(), true);
+        return view('welcome', ['body' => $body]);
+    }
 }
--- app/routes/web.php
+++ app/routes/web.php
@@ -1,19 +1,3 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-Route::get('/', function () {
-    $client = new \GuzzleHttp\Client();
-    $res = $client->request('POST', 'http://compiler:4567');
-    $body = json_decode($res->getBody(), true);
-    return view('welcome', ['body' => $body]);
-});
+Route::get('/', 'IndexController@index');
