893841eb029ad8d30cc04f538469e861992cdd96
David Robertson <dmichaelrobertson@gmail.com>
2017-11-12 09:21:05 +0000

    Compile assets.

--- app/public/js/send-execute-request.js
+++ app/public/js/send-execute-request.js
@@ -138,45 +138,23 @@ function drawTree(data) {
         }
     });
 
-    // set the dimensions and margins of the diagram
     var margin = { top: 35, right: 10, bottom: 35, left: 10 };
     var width = 770 - margin.left - margin.right;
     var height = 800 - margin.top - margin.bottom;
-
-    // declares a tree layout and assigns the size
     var treemap = d3.tree().size([width, height]);
-
-    // assigns the data to a hierarchy using parent-child relationships
     var nodes = d3.hierarchy(treeData[0]);
-
-    // maps the node data to the tree layout
     nodes = treemap(nodes);
-
-    // append the svg obgect to the body of the page
-    // appends a 'group' element to 'svg'
-    // moves the 'group' element to the top left margin
     var svg = d3.select(".program-tree").append("div").classed("svg-container", true).append("svg").attr("preserveAspectRatio", "xMinYMin meet").attr("viewBox", "0 0 770 800").classed("svg-content-responsive", true);
-    // var svg = d3.select(".program-tree").append("svg")
-    //     .attr("width", width + margin.left + margin.right)
-    //     .attr("height", height + margin.top + margin.bottom);
     var g = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-    // adds the links between the nodes
     var link = g.selectAll(".link").data(nodes.descendants().slice(1)).enter().append("path").attr("class", "link").attr("d", function (d) {
         return "M" + d.x + "," + d.y + "C" + d.x + "," + (d.y + d.parent.y) / 2 + " " + d.parent.x + "," + (d.y + d.parent.y) / 2 + " " + d.parent.x + "," + d.parent.y;
     });
-
-    // adds each node as a group
     var node = g.selectAll(".node").data(nodes.descendants()).enter().append("g").attr("class", function (d) {
         return "node" + (d.children ? " node--internal" : " node--leaf");
     }).attr("transform", function (d) {
         return "translate(" + d.x + "," + d.y + ")";
     });
-
-    // adds the circle to the node
     node.append("circle").attr("r", 5);
-
-    // adds the text to the node
     node.append("text").attr("dy", ".35em").attr("y", function (d) {
         return d.children ? -20 : 20;
     }).style("text-anchor", "middle").text(function (d) {
