1dd9e9dacbbbdf50da9ee785013b44d550db0771
David Robertson <dmichaelrobertson@gmail.com>
2017-11-14 15:34:37 +0000

    Follow more appropriate Javascript style guides and reformat code.

--- app/resources/assets/js/send-execute-request.js
+++ app/resources/assets/js/send-execute-request.js
@@ -1,47 +1,49 @@
-$('#execute-form').submit(function(e) {
+"use strict";
+
+$("#execute-form").submit(function(e) {
     // Get the form that was submitted
     var $form = $(this);
     // Stop the form submitting normally (i.e., don't route to action parameter)
     e.preventDefault();
     // Get the intended controller route
-    var url = $form.attr('action');
+    var url = $form.attr("action");
     // Get csrf token from page meta-data
-    var AUTH_TOKEN = $('meta[name="csrf-token"]').attr('content');
+    var AUTH_TOKEN = $("meta[name='csrf-token']").attr("content");
     // Serialise the form inputs, add csrf token
     var data = $form.serialize() + "&_token=" + AUTH_TOKEN;
     // Post to the controller
-    $.post(url, data, function(response) {
-        var response = response['response'];
-        var numSyntaxErrors = response['numSyntaxErrors'];
-        var syntaxErrors = response['syntaxErrors'];
-        var numContextualErrors = response['numContextualErrors'];
-        var contextualErrors = response['contextualErrors'];
-        var astData = response['astData'];
-        var objectCode = response['objectCode'];
-        var output = response['output']
-        $('.program-tree').text("");
+    $.post(url, data, function(responseData) {
+        var response = responseData.response;
+        var numSyntaxErrors = response.numSyntaxErrors;
+        var syntaxErrors = response.syntaxErrors;
+        var numContextualErrors = response.numContextualErrors;
+        var contextualErrors = response.contextualErrors;
+        var astData = response.astData;
+        var objectCode = response.objectCode;
+        var output = response.output;
+        $(".program-tree").text("");
         if (numSyntaxErrors > 0) {
-            $('.program-tree').append("Number of syntax errors: " + numSyntaxErrors + "<br>");
-            $('.program-tree').append("Syntax errors: <br>");
+            $(".program-tree").append("Number of syntax errors: " + numSyntaxErrors + "<br>");
+            $(".program-tree").append("Syntax errors: <br>");
             $.each(syntaxErrors, function(index, syntaxError) {
-                $('.program-tree').append((index+1) + ": " + syntaxError);
+                $(".program-tree").append((index + 1) + ": " + syntaxError);
             });
-            $('.program-tree').append("<br>");
+            $(".program-tree").append("<br>");
         } else if (numContextualErrors > 0) {
-            $('.program-tree').append("Number of contextual errors: " + numContextualErrors + "<br>");
-            $('.program-tree').append("Contextual errors: <br>");
+            $(".program-tree").append("Number of contextual errors: " + numContextualErrors + "<br>");
+            $(".program-tree").append("Contextual errors: <br>");
             $.each(contextualErrors, function(index, contextualError) {
-                $('.program-tree').append((index+1) + ": " + contextualError);
+                $(".program-tree").append((index + 1) + ": " + contextualError);
             });
-            $('.program-tree').append("<br>");
+            $(".program-tree").append("<br>");
         } else {
             drawTree(astData);
         }
-        $('.object-code').text("");
+        $(".object-code").text("");
         $.each(objectCode, function(index, instruction) {
-            $('.object-code').append(instruction + "<br>");
+            $(".object-code").append(instruction + "<br>");
         });
-        $('.output').text(output);
+        $(".output").text(output);
     });
 });
 
@@ -60,54 +62,61 @@ function drawTree(data) {
         }
     });
 
-    var margin = {top: 35, right: 10, bottom: 35, left: 10};
+    var margin = {
+        top: 35,
+        right: 10,
+        bottom: 35,
+        left: 10
+    };
     var width = 770 - margin.left - margin.right;
     var height = 800 - margin.top - margin.bottom;
     var treemap = d3.tree().size([width, height]);
     var nodes = d3.hierarchy(treeData[0]);
     nodes = treemap(nodes);
     var svg = d3.select(".program-tree")
-    .append("div")
-    .classed("svg-container", true)
-    .append("svg")
-    .attr("preserveAspectRatio", "xMinYMin meet")
-    .attr("viewBox", "0 0 770 800")
-    .classed("svg-content-responsive", true);
+        .append("div")
+        .classed("svg-container", true)
+        .append("svg")
+        .attr("preserveAspectRatio", "xMinYMin meet")
+        .attr("viewBox", "0 0 770 800")
+        .classed("svg-content-responsive", true);
     var g = svg.append("g")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-    var link = g.selectAll(".link")
-    .data( nodes.descendants().slice(1)).enter().append("path")
-    .attr("class", "link").attr("d", function(d) {
-        return "M" + d.x + "," + d.y
-        + "C" + d.x + "," + (d.y + d.parent.y) / 2
-        + " " + d.parent.x + "," +  (d.y + d.parent.y) / 2
-        + " " + d.parent.x + "," + d.parent.y;
-    });
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+    g.selectAll(".link")
+        .data(nodes.descendants().slice(1)).enter().append("path")
+        .attr("class", "link").attr("d", function(d) {
+            return "M" + d.x + "," + d.y +
+                "C" + d.x + "," + (d.y + d.parent.y) / 2 +
+                " " + d.parent.x + "," + (d.y + d.parent.y) / 2 +
+                " " + d.parent.x + "," + d.parent.y;
+        });
     var node = g.selectAll(".node")
-    .data(nodes.descendants())
-    .enter().append("g")
-    .attr("class", function(d) {
-        return "node" + (d.children ? " node--internal" : " node--leaf");
-    }).attr("transform", function(d) {
-        return "translate(" + d.x + "," + d.y + ")";
-    }).attr("id", function(d) {
-        return "node-" + d.data.id;
-    });
+        .data(nodes.descendants())
+        .enter().append("g")
+        .attr("class", function(d) {
+            return "node" + (d.children ? " node--internal" : " node--leaf");
+        }).attr("transform", function(d) {
+            return "translate(" + d.x + "," + d.y + ")";
+        }).attr("id", function(d) {
+            return "node-" + d.data.id;
+        });
     node.append("circle").attr("r", 5);
     node.append("text")
-    .attr("dy", ".35em")
-    .attr("y", function(d) { return d.children ? -20 : 20; })
-    .style("text-anchor", "middle")
-    .text(function(d) { return d.data.name; });
+        .attr("dy", ".35em")
+        .attr("y", function(d) {
+            return d.children ? -20 : 20;
+        })
+        .style("text-anchor", "middle")
+        .text(function(d) {
+            return d.data.name;
+        });
 
     animateTree(data);
 }
 
 function animateTree(data) {
-    var counter = 0;
-    $.each(data, function () {
-        d3.select("#node-"+this.id).select("circle").transition()
-        .duration(500).delay(500*counter).style("fill", "red");
-        counter++;
+    $.each(data, function(index) {
+        d3.select("#node-" + this.id).select("circle").transition()
+            .duration(500).delay(500 * index).style("fill", "red");
     });
 }
