014bb594ceace7ec475292423df703c2e8483de6
David Robertson <dmichaelrobertson@gmail.com>
2017-12-02 16:14:42 +0000

    Use a much better system for requiring CodeMirror modes.

--- app/resources/assets/js/app.js
+++ app/resources/assets/js/app.js
@@ -1,3 +1,7 @@
 require('./bootstrap');
 
 window.d3 = require('d3');
+
+window.CodeMirror = require('codemirror');
+
+require('codemirror/mode/javascript/javascript');
--- app/resources/assets/js/codemirror.js
+++ app/resources/assets/js/codemirror.js
@@ -1,12 +1,8 @@
 "use strict";
 
-require([
-    "codemirror/lib/codemirror", "codemirror/mode/javascript/javascript"
-], function(CodeMirror) {
-    // Note that you cannot use the JQuery DOM selector when using CodeMirror
-    CodeMirror.fromTextArea(document.getElementById("code-editor"), {
-        lineNumbers: true,
-        mode: "javascript",
-        theme: "ambiance"
-    });
+// Note that you cannot use the JQuery DOM selector when using CodeMirror
+CodeMirror.fromTextArea(document.getElementById("code-editor"), {
+    lineNumbers: true,
+    mode: "javascript",
+    theme: "ambiance"
 });
