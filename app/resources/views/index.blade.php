bbdd9daa0e740fcae0f07e1c4c0b9560d49b3768
David Robertson <dmichaelrobertson@gmail.com>
2017-12-04 22:43:23 +0000

    Add playback glyphicons.

--- app/resources/views/index.blade.php
+++ app/resources/views/index.blade.php
@@ -26,10 +26,10 @@
                 <div class="center-container">
                     <div class="controls-container">
                         <!-- change how styling works here -->
-                        <p class="pull-left" id="reverse-button" ><< |</p>
-                        <p class="pull-left" id="play-button">| Play |</p>
-                        <p class="pull-left" id="pause-button" hidden>| Pause |</p>
-                        <p class="pull-left" id="forward-button" >| >></p>
+                        <i id="reverse-button" class="pull-left glyphicon glyphicon-step-backward"></i>
+                        <i id="play-button" class="pull-left glyphicon glyphicon-play"></i>
+                        <i id="pause-button" class="pull-left glyphicon glyphicon-pause"></i>
+                        <i id="forward-button" class="pull-left glyphicon glyphicon-step-forward"></i>
                         <p class="pull-right">Contextual-analysis | Code-generation</p>
                     </div>
                     <div class="program-tree-container">
