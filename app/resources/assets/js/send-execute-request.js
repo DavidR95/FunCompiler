95d43102c9d20df6430908c66e3bd5dc5d7b563b
David Robertson <dmichaelrobertson@gmail.com>
2017-12-05 18:17:20 +0000

    Fix issues relating to stepping backwards or forwards when the animation is either finished or not yet started.

--- app/resources/assets/js/send-execute-request.js
+++ app/resources/assets/js/send-execute-request.js
@@ -1,6 +1,6 @@
 "use strict";
 
-var currentNodeIndex = 0;
+var currentNodeIndex = -1;
 var is_playing = false;
 
 $("#execute-form").submit(function(e) {
@@ -42,7 +42,7 @@ $("#execute-form").submit(function(e) {
 });
 
 function drawTree(data, contextualNodeOrder) {
-    currentNodeIndex = 0;
+    currentNodeIndex = -1;
     var dataMap = data.reduce(function(map, node) {
         map[node.id] = node;
         return map;
@@ -125,7 +125,7 @@ function drawTree(data, contextualNodeOrder) {
     });
 }
 
-function animateNode(node, currentNode, delayOffset) {
+function animateNode(node, currentNode, delayOffset, numNodes) {
     d3.select("#node-" + node.id).select("rect").transition()
         .duration(500).delay(delayOffset * 1000).style("fill", "yellow")
         .on("start", function() {
@@ -141,13 +141,17 @@ function animateNode(node, currentNode, delayOffset) {
             $.each(node.explanations, function(index, explanation) {
                 $(".explanations").append(explanation + "<br>");
             });
+        }).on("end", function() {
+            if (currentNode === numNodes-1)
+                is_playing = false;
         }).transition().style("fill", "white");
 }
 
 function animateTree(nodeOrder) {
+    currentNodeIndex = (currentNodeIndex == -1 ? 0 : currentNodeIndex);
     for (var i = currentNodeIndex, j = 0; i < nodeOrder.length; i++, j++) {
         var node = nodeOrder[i];
-        animateNode(node, i, j);
+        animateNode(node, i, j, nodeOrder.length);
     }
 }
 
@@ -172,12 +176,12 @@ function forward(nodeOrder) {
     if (is_playing)
         pause();
     var node = nodeOrder[currentNodeIndex+1];
-    animateNode(node, currentNodeIndex+1, 0);
+    animateNode(node, currentNodeIndex+1, 0, nodeOrder.length);
 }
 
 function reverse(nodeOrder) {
     if (is_playing)
         pause();
     var node = nodeOrder[currentNodeIndex-1];
-    animateNode(node, currentNodeIndex-1, 0);
+    animateNode(node, currentNodeIndex-1, 0, nodeOrder.length);
 }
