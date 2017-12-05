1e6114dbfdd27c164c39c9d1928f666e0d05641e
David Robertson <dmichaelrobertson@gmail.com>
2017-12-05 12:24:34 +0000

    Add light styling to new rectangular nodes.

--- app/resources/assets/js/send-execute-request.js
+++ app/resources/assets/js/send-execute-request.js
@@ -95,12 +95,13 @@ function drawTree(data, contextualNodeOrder) {
         }).attr("id", function(d) {
             return "node-" + d.data.id;
         });
-    node.append("rect").attr("x", -25).attr("y", -12).attr("width", 50).attr("height", 25);
+    node.append("rect")
+        .attr("x", -25)
+        .attr("y", -12.5)
+        .attr("width", 50)
+        .attr("height", 25);
     node.append("text")
         .attr("dy", ".35em")
-        // .attr("y", function(d) {
-        //     return d.children ? -20 : 20;
-        // })
         .style("text-anchor", "middle")
         .text(function(d) {
             return d.data.name;
@@ -121,8 +122,8 @@ function drawTree(data, contextualNodeOrder) {
 }
 
 function animateNode(node, currentNode, delayOffset) {
-    d3.select("#node-" + node.id).select("circle").transition()
-        .duration(500).delay(delayOffset * 1000).style("fill", "red")
+    d3.select("#node-" + node.id).select("rect").transition()
+        .duration(500).delay(delayOffset * 1000).style("fill", "yellow")
         .on("start", function() {
             currentNodeIndex = currentNode;
             $(".typeTable tbody").text("");
@@ -157,7 +158,7 @@ function pause() {
     is_playing = false;
     $("#play-button").show();
     $("#pause-button").hide();
-    d3.selectAll("circle").interrupt();
+    d3.selectAll("rect").interrupt();
 }
 
 function forward(nodeOrder) {
