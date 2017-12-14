945b0489c9f8e4ddae32cddc59cdfd6b9a911e8d
David Robertson <dmichaelrobertson@gmail.com>
2017-12-14 16:57:45 +0000

    Add some initial keyword rules to the mode.

--- app/public/js/app.js
+++ app/public/js/app.js
@@ -31706,17 +31706,9 @@ var CodeMirror = __webpack_require__(90);
 __webpack_require__(484);
 
 CodeMirror.defineSimpleMode("simplemode", {
-  start: [
-  // The regex matches the token, the token property contains the type
-  { regex: /"(?:[^\\]|\\.)*?(?:"|$)/, token: "string" },
-  // You can match multiple tokens at once. Note that the captured
-  // groups must span the whole string in this case
-  { regex: /(function)(\s+)([a-z$][\w$]*)/,
-    token: ["keyword", null, "variable-2"] },
-  // Rules are matched in the order in which they appear, so there is
-  // no ambiguity between this one and the one above
-  { regex: /(?:function|var|return|if|for|while|else|do|this)\b/,
-    token: "keyword" }, { regex: /true|false|null|undefined/, token: "atom" }, { regex: /0x[a-f\d]+|[-+]?(?:\.\d+|\d+\.?\d*)(?:e[-+]?\d+)?/i,
+  start: [{ regex: /"(?:[^\\]|\\.)*?(?:"|$)/, token: "string" }, { regex: /(function)(\s+)([a-z$][\w$]*)/,
+    token: ["keyword", null, "variable-2"] }, { regex: /(?:func|proc|return|if|while|else)\b/,
+    token: "keyword" }, { regex: /true|false/, token: "atom" }, { regex: /0x[a-f\d]+|[-+]?(?:\.\d+|\d+\.?\d*)(?:e[-+]?\d+)?/i,
     token: "number" }, { regex: /\/\/.*/, token: "comment" }, { regex: /\/(?:[^\\]|\\.)*?\//, token: "variable-3" },
   // A next property will cause the mode to move to a different state
   { regex: /\/\*/, token: "comment", next: "comment" }, { regex: /[-+\/*=<>!]+/, token: "operator" },
@@ -31726,16 +31718,7 @@ CodeMirror.defineSimpleMode("simplemode", {
   // causes all code between << and >> to be highlighted with the XML
   // mode.
   { regex: /<</, token: "meta", mode: { spec: "xml", end: />>/ } }],
-  // The multi-line comment state.
-  comment: [{ regex: /.*?\*\//, token: "comment", next: "start" }, { regex: /.*/, token: "comment" }],
-  // The meta property contains global information about the mode. It
-  // can contain properties like lineComment, which are supported by
-  // all modes, and also directives like dontIndentStates, which are
-  // specific to simple modes.
-  meta: {
-    dontIndentStates: ["comment"],
-    lineComment: "//"
-  }
+  comment: []
 });
 
 // Note that you cannot use the JQuery DOM selector when using CodeMirror
--- app/resources/assets/js/codemirror.js
+++ app/resources/assets/js/codemirror.js
@@ -6,17 +6,12 @@ require('codemirror/addon/mode/simple');
 
 CodeMirror.defineSimpleMode("simplemode", {
   start: [
-    // The regex matches the token, the token property contains the type
     {regex: /"(?:[^\\]|\\.)*?(?:"|$)/, token: "string"},
-    // You can match multiple tokens at once. Note that the captured
-    // groups must span the whole string in this case
     {regex: /(function)(\s+)([a-z$][\w$]*)/,
      token: ["keyword", null, "variable-2"]},
-    // Rules are matched in the order in which they appear, so there is
-    // no ambiguity between this one and the one above
-    {regex: /(?:function|var|return|if|for|while|else|do|this)\b/,
+    {regex: /(?:func|proc|return|if|while|else)\b/,
      token: "keyword"},
-    {regex: /true|false|null|undefined/, token: "atom"},
+    {regex: /true|false/, token: "atom"},
     {regex: /0x[a-f\d]+|[-+]?(?:\.\d+|\d+\.?\d*)(?:e[-+]?\d+)?/i,
      token: "number"},
     {regex: /\/\/.*/, token: "comment"},
@@ -33,19 +28,7 @@ CodeMirror.defineSimpleMode("simplemode", {
     // mode.
     {regex: /<</, token: "meta", mode: {spec: "xml", end: />>/}}
   ],
-  // The multi-line comment state.
-  comment: [
-    {regex: /.*?\*\//, token: "comment", next: "start"},
-    {regex: /.*/, token: "comment"}
-  ],
-  // The meta property contains global information about the mode. It
-  // can contain properties like lineComment, which are supported by
-  // all modes, and also directives like dontIndentStates, which are
-  // specific to simple modes.
-  meta: {
-    dontIndentStates: ["comment"],
-    lineComment: "//"
-  }
+  comment: []
 });
 
 // Note that you cannot use the JQuery DOM selector when using CodeMirror
