c7bd50796a3d7142ceda72d5644b325049b5e655
David Robertson <dmichaelrobertson@gmail.com>
2017-12-19 11:32:36 +0000

    Add another test for successful code generation requests.

--- app/tests/Feature/HttpTest.php
+++ app/tests/Feature/HttpTest.php
@@ -3,17 +3,16 @@
 namespace Tests\Feature;
 
 use Tests\TestCase;
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithoutMiddleware;
 
 class HttpTest extends TestCase
 {
     /**
-     * A basic test example.
+     * Make a post request for a contextual analysis result and ensure only
+     * the correct fragments are returned.
      *
      * @return void
      */
-    public function testExecuteRequest()
+    public function testSuccessfulCARequest()
     {
         $response = $this->json('POST', route('execute', [
             'type' => 'ca',
@@ -23,8 +22,39 @@ class HttpTest extends TestCase
         $response->assertSuccessful()
                  ->assertJsonFragment([
                      'redirect_url' => '/',
-                     'numSyntaxErrors' => 0
-                 ]);
+                     'numSyntaxErrors' => 0,
+                     'numContextualErrors' => 0,
+                     'syntaxErrors' => [],
+                     'contextualErrors' => []
+                 ])
+                 ->assertSeeText('nodeOrder')
+                 ->assertSeeText('treeNodes')
+                 ->assertDontSeeText('objectCode');
     }
 
+    /**
+     * Make a post request for a contextual analysis result and ensure only
+     * the correct fragments are returned.
+     *
+     * @return void
+     */
+    public function testSuccessfulCGRequest()
+    {
+        $response = $this->json('POST', route('execute', [
+            'type' => 'cg',
+            'program' => 'int n = 15 proc main(): while n > 1: n = n/2 . .'
+        ]));
+
+        $response->assertSuccessful()
+                 ->assertJsonFragment([
+                     'redirect_url' => '/',
+                     'numSyntaxErrors' => 0,
+                     'numContextualErrors' => 0,
+                     'syntaxErrors' => [],
+                     'contextualErrors' => []
+                 ])
+                 ->assertSeeText('nodeOrder')
+                 ->assertSeeText('treeNodes')
+                 ->assertSeeText('objectCode');
+    }
 }
