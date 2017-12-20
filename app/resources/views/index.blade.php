acc88c11e2051324f030d3d0f6881c27c1e9fcf4
David Robertson <dmichaelrobertson@gmail.com>
2017-12-20 11:42:15 +0000

    Override several bootstrap sass variables.

--- app/resources/views/index.blade.php
+++ app/resources/views/index.blade.php
@@ -8,7 +8,7 @@
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" href="#">FunCompiler</a>
+            <a class="navbar-brand" href="#">Fun<b>Compiler</b></a>
         </div>
         <div id="navbar" class="navbar-collapse collapse">
             <ul class="nav navbar-nav navbar-right">
