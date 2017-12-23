6c9f1c40c46180c853c636da0ffb226a1be81211
David Robertson <dmichaelrobertson@gmail.com>
2017-12-23 18:03:14 +0000

    Logo navigates back to home page.

--- app/resources/views/index.blade.php
+++ app/resources/views/index.blade.php
@@ -2,13 +2,15 @@
 @section('content')
     <nav class="navbar navbar-default">
         <div class="navbar-header">
-            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <button type="button" class="navbar-toggle collapsed"
+                    data-toggle="collapse" data-target="#navbar"
+                    aria-expanded="false" aria-controls="navbar">
                 <span class="sr-only">Toggle navigation</span>
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" href="#">Fun<b>Compiler</b></a>
+            <a class="navbar-brand" href="/">Fun<b>Compiler</b></a>
         </div>
         <div id="navbar" class="navbar-collapse collapse">
             <ul class="nav navbar-nav navbar-right">
