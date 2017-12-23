fd74d9481465a3cf6e142f5a7d87cd38e89be902
David Robertson <dmichaelrobertson@gmail.com>
2017-12-23 17:56:50 +0000

    Load css via the same method.

--- app/public/js/app.js
+++ app/public/js/app.js
@@ -18973,8 +18973,8 @@ function transform(node) {
 /***/ (function(module, exports, __webpack_require__) {
 
 __webpack_require__(173);
-__webpack_require__(475);
-module.exports = __webpack_require__(476);
+__webpack_require__(476);
+module.exports = __webpack_require__(477);
 
 
 /***/ }),
@@ -32078,7 +32078,7 @@ var CodeSnippets = module.exports = {
  * ========================================================================== */
 
 // Import Code Animation module
-var CodeAnimation = __webpack_require__(489);
+var CodeAnimation = __webpack_require__(182);
 
 // Indicates which button was pressed, contextual analysis or code generation
 var executionType;
@@ -32146,7 +32146,235 @@ $("#execute-form").submit(function (e) {
 });
 
 /***/ }),
-/* 182 */,
+/* 182 */
+/***/ (function(module, exports, __webpack_require__) {
+
+/* ==========================================================================
+ * codeAnimation.js
+ *
+ * Build and draw the AST using the D3 library.
+ *
+ * Define the logic used to traverse/animate the resulting AST.
+ * ========================================================================== */
+
+// Import the D3 node module
+var d3 = __webpack_require__(183);
+// Import CodeTemplates module
+var CodeTemplates = __webpack_require__(474);
+// Import CodeHelpers module
+var CodeHelpers = __webpack_require__(475);
+
+var CodeAnimation = module.exports = {
+    // Draw the AST gives the tree nodes
+    drawTree: function drawTree(data) {
+        var treeData = CodeHelpers.buildTree(data);
+        var marginLeft = 10;
+        var marginTop = 35;
+        var width = 800 - marginLeft * 2;
+        var height = 650 - marginTop * 2;
+        var treemap = d3.tree().size([width, height]);
+        var nodes = d3.hierarchy(treeData[0]);
+        nodes = treemap(nodes);
+        var svg = d3.select(".program-tree-container").html("").append("div").classed("svg-container", true).append("svg").attr("preserveAspectRatio", "none").attr("viewBox", "0 0 800 650");
+        var g = svg.append("g").attr("transform", "translate(" + marginLeft + "," + marginTop + ")");
+        g.selectAll(".link").data(nodes.descendants().slice(1)).enter().append("path").attr("class", "link").attr("d", function (d) {
+            return "M" + d.x + "," + d.y + "C" + d.x + "," + (d.y + d.parent.y) / 2 + " " + d.parent.x + "," + (d.y + d.parent.y) / 2 + " " + d.parent.x + "," + d.parent.y;
+        });
+        var node = g.selectAll(".node").data(nodes.descendants()).enter().append("g").attr("class", function (d) {
+            return "node" + (d.children ? " node--internal" : " node--leaf");
+        }).attr("transform", function (d) {
+            return "translate(" + d.x + "," + d.y + ")";
+        }).attr("id", function (d) {
+            return "node-" + d.data.id;
+        }).attr("data-node-name", function (d) {
+            return d.data.nodeName;
+        }).attr("data-node-value", function (d) {
+            return d.data.nodeValue;
+        });
+        node.append("text").attr("dy", ".35em").style("text-anchor", "middle").style("font-size", "0.75em").text(function (d) {
+            var name = d.data.nodeValue;
+            if (name.length <= 5) return name;else return name.substring(0, 5) + "...";
+        });
+        node.each(function () {
+            var node = d3.select(this);
+            var bBox = node.select("text").node().getBBox();
+            node.insert("rect", ":first-child").attr("x", bBox.x - 3).attr("y", bBox.y - 3).attr("height", bBox.height + 6).attr("width", bBox.width + 6);
+        });
+    },
+
+    // Stop any currently running animation and show the correct containers
+    initialise: function initialise(executionType, executionNodeOrder) {
+        pause();
+        previousNode = null;
+        currentNodeIndex = -1;
+        showGenerationAnimation = executionType === "cg" ? true : false;
+        if (showGenerationAnimation) {
+            $(".controls-container span").html("Code Generation");
+            $("#display-contextual-container").hide();
+            $("#display-generation-container").show();
+        } else {
+            $(".controls-container span").html("Contextual Analysis");
+            $("#display-generation-container").hide();
+            $("#display-contextual-container").show();
+        }
+        nodeOrder = executionNodeOrder;
+    },
+
+    // 'Animate' the first node in the tree
+    highlightFirstNode: function highlightFirstNode() {
+        var node = nodeOrder[currentNodeIndex + 1];
+        animateNode(node, true, 0);
+    }
+};
+
+var nodeOrder;
+var currentNodeIndex;
+var is_playing;
+var showGenerationAnimation;
+var previousNode;
+
+$("#play-button").on("click", function () {
+    play();
+});
+$("#pause-button").on("click", function () {
+    pause();
+});
+$("#forward-button").on("click", function () {
+    forward();
+});
+$("#reverse-button").on("click", function () {
+    reverse();
+});
+
+// Highlights a single node and displays any corresponding information
+function animateNode(node, isPlayingForward, delayOffset) {
+    if (showGenerationAnimation) {
+        var explanationsText = $(".generation-explanations ul");
+        var objectCodeText = $(".object-code ul");
+        var tableBody = $(".address-table tbody");
+        var tableWrapper = $(".address-table").parent();
+        var codeTemplateText = $(".code-template ul");
+    } else {
+        var explanationsText = $(".contextual-explanations ul");
+        var tableBody = $(".type-table tbody");
+        var tableWrapper = $(".type-table").parent();
+    }
+    d3.select("#node-" + node.id).select("rect").transition().duration(0).delay(delayOffset * 1000).style("fill", "#035a80").on("start", function () {
+        $(this).next("text").css({ "font-weight": "900" });
+        if (previousNode != null && previousNode !== this) {
+            $(previousNode).css("fill", "#3e4153");
+            $(previousNode).next("text").css({
+                "fill": "white",
+                "font-weight": "normal"
+            });
+        }
+
+        isPlayingForward ? currentNodeIndex++ : currentNodeIndex--;
+
+        var nodeName = $("#node-" + node.id).data("node-name");
+
+        $(".data-heading-container span").html(nodeName);
+
+        var tableEntries = "";
+        $.each(node.table, function (index, tableEntry) {
+            tableEntries += "<tr><td>" + tableEntry.scope + "</td><td>" + tableEntry.id + "</td><td>" + tableEntry.type_address + "</td></tr>";
+        });
+        tableBody.html(tableEntries);
+        tableWrapper.scrollTop(tableWrapper.prop("scrollHeight"));
+
+        var explanations = "";
+        $.each(node.explanations, function (index, explanation) {
+            explanations += "<li>> " + explanation + "</li>";
+        });
+        explanationsText.html(explanations);
+        explanationsText.scrollTop(explanationsText.prop("scrollHeight"));
+
+        if (showGenerationAnimation) {
+            var objectCodeInstructions = "";
+            $.each(node.objectCode, function (index, objectCode) {
+                objectCodeInstructions += "<li>" + objectCode + "</li>";
+            });
+            objectCodeText.html(objectCodeInstructions);
+            var codeTemplateInstructions = "";
+            var codeTemplate = CodeTemplates.getTemplate(nodeName);
+            $.each(codeTemplate, function (index, codeTemplateInstruction) {
+                codeTemplateInstructions += "<li>> " + codeTemplateInstruction + "</li>";
+            });
+            codeTemplateText.html(codeTemplateInstructions);
+        }
+    }).on("end", function () {
+        previousNode = this;
+        if (hasAnimationFinished() && is_playing) {
+            is_playing = false;
+            enablePlayButton();
+        }
+    });
+}
+
+// Animates each node sequentially
+function animateTree() {
+    for (var i = currentNodeIndex, j = 0; i < nodeOrder.length - 1; i++, j++) {
+        var node = nodeOrder[i + 1];
+        animateNode(node, true, j);
+    }
+}
+
+// Start playing the animation, restart if already at the end
+function play() {
+    is_playing = true;
+    enablePauseButton();
+    if (hasAnimationFinished()) currentNodeIndex = -1;
+    animateTree();
+}
+
+// Pause the animation, interrupt all current animations
+function pause() {
+    is_playing = false;
+    enablePlayButton();
+    d3.selectAll("rect").interrupt();
+}
+
+// Move one node forward
+function forward() {
+    if (is_playing) pause();
+    if (!hasAnimationFinished()) {
+        var node = nodeOrder[currentNodeIndex + 1];
+        animateNode(node, true, 0);
+    }
+}
+
+// Move one node backward
+function reverse() {
+    if (is_playing) pause();
+    if (hasAnimationStarted()) {
+        var node = nodeOrder[currentNodeIndex - 1];
+        animateNode(node, false, 0);
+    }
+}
+
+// Show the play button, hide the pause button
+function enablePlayButton() {
+    $("#play-button").show();
+    $("#pause-button").hide();
+}
+
+// Show the pause button, hide the play button
+function enablePauseButton() {
+    $("#play-button").hide();
+    $("#pause-button").show();
+}
+
+// Check if the animation has reached the last node
+function hasAnimationFinished() {
+    return currentNodeIndex === nodeOrder.length - 1;
+}
+
+// Check if the animation has moved beyond the first node
+function hasAnimationStarted() {
+    return currentNodeIndex > 0;
+}
+
+/***/ }),
 /* 183 */
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
@@ -45753,259 +45981,6 @@ var CodeTemplates = module.exports = {
 /* 475 */
 /***/ (function(module, exports) {
 
-// removed by extract-text-webpack-plugin
-
-/***/ }),
-/* 476 */
-/***/ (function(module, exports) {
-
-// removed by extract-text-webpack-plugin
-
-/***/ }),
-/* 477 */,
-/* 478 */,
-/* 479 */,
-/* 480 */,
-/* 481 */,
-/* 482 */,
-/* 483 */,
-/* 484 */,
-/* 485 */,
-/* 486 */,
-/* 487 */,
-/* 488 */,
-/* 489 */
-/***/ (function(module, exports, __webpack_require__) {
-
-/* ==========================================================================
- * codeAnimation.js
- *
- * Build and draw the AST using the D3 library.
- *
- * Define the logic used to traverse/animate the resulting AST.
- * ========================================================================== */
-
-// Import the D3 node module
-var d3 = __webpack_require__(183);
-// Import CodeTemplates module
-var CodeTemplates = __webpack_require__(474);
-// Import CodeHelpers module
-var CodeHelpers = __webpack_require__(490);
-
-var CodeAnimation = module.exports = {
-    // Draw the AST gives the tree nodes
-    drawTree: function drawTree(data) {
-        var treeData = CodeHelpers.buildTree(data);
-        var marginLeft = 10;
-        var marginTop = 35;
-        var width = 800 - marginLeft * 2;
-        var height = 650 - marginTop * 2;
-        var treemap = d3.tree().size([width, height]);
-        var nodes = d3.hierarchy(treeData[0]);
-        nodes = treemap(nodes);
-        var svg = d3.select(".program-tree-container").html("").append("div").classed("svg-container", true).append("svg").attr("preserveAspectRatio", "none").attr("viewBox", "0 0 800 650");
-        var g = svg.append("g").attr("transform", "translate(" + marginLeft + "," + marginTop + ")");
-        g.selectAll(".link").data(nodes.descendants().slice(1)).enter().append("path").attr("class", "link").attr("d", function (d) {
-            return "M" + d.x + "," + d.y + "C" + d.x + "," + (d.y + d.parent.y) / 2 + " " + d.parent.x + "," + (d.y + d.parent.y) / 2 + " " + d.parent.x + "," + d.parent.y;
-        });
-        var node = g.selectAll(".node").data(nodes.descendants()).enter().append("g").attr("class", function (d) {
-            return "node" + (d.children ? " node--internal" : " node--leaf");
-        }).attr("transform", function (d) {
-            return "translate(" + d.x + "," + d.y + ")";
-        }).attr("id", function (d) {
-            return "node-" + d.data.id;
-        }).attr("data-node-name", function (d) {
-            return d.data.nodeName;
-        }).attr("data-node-value", function (d) {
-            return d.data.nodeValue;
-        });
-        node.append("text").attr("dy", ".35em").style("text-anchor", "middle").style("font-size", "0.75em").text(function (d) {
-            var name = d.data.nodeValue;
-            if (name.length <= 5) return name;else return name.substring(0, 5) + "...";
-        });
-        node.each(function () {
-            var node = d3.select(this);
-            var bBox = node.select("text").node().getBBox();
-            node.insert("rect", ":first-child").attr("x", bBox.x - 3).attr("y", bBox.y - 3).attr("height", bBox.height + 6).attr("width", bBox.width + 6);
-        });
-    },
-
-    // Stop any currently running animation and show the correct containers
-    initialise: function initialise(executionType, executionNodeOrder) {
-        pause();
-        previousNode = null;
-        currentNodeIndex = -1;
-        showGenerationAnimation = executionType === "cg" ? true : false;
-        if (showGenerationAnimation) {
-            $(".controls-container span").html("Code Generation");
-            $("#display-contextual-container").hide();
-            $("#display-generation-container").show();
-        } else {
-            $(".controls-container span").html("Contextual Analysis");
-            $("#display-generation-container").hide();
-            $("#display-contextual-container").show();
-        }
-        nodeOrder = executionNodeOrder;
-    },
-
-    // 'Animate' the first node in the tree
-    highlightFirstNode: function highlightFirstNode() {
-        var node = nodeOrder[currentNodeIndex + 1];
-        animateNode(node, true, 0);
-    }
-};
-
-var nodeOrder;
-var currentNodeIndex;
-var is_playing;
-var showGenerationAnimation;
-var previousNode;
-
-$("#play-button").on("click", function () {
-    play();
-});
-$("#pause-button").on("click", function () {
-    pause();
-});
-$("#forward-button").on("click", function () {
-    forward();
-});
-$("#reverse-button").on("click", function () {
-    reverse();
-});
-
-// Highlights a single node and displays any corresponding information
-function animateNode(node, isPlayingForward, delayOffset) {
-    if (showGenerationAnimation) {
-        var explanationsText = $(".generation-explanations ul");
-        var objectCodeText = $(".object-code ul");
-        var tableBody = $(".address-table tbody");
-        var tableWrapper = $(".address-table").parent();
-        var codeTemplateText = $(".code-template ul");
-    } else {
-        var explanationsText = $(".contextual-explanations ul");
-        var tableBody = $(".type-table tbody");
-        var tableWrapper = $(".type-table").parent();
-    }
-    d3.select("#node-" + node.id).select("rect").transition().duration(0).delay(delayOffset * 1000).style("fill", "#035a80").on("start", function () {
-        $(this).next("text").css({ "font-weight": "900" });
-        if (previousNode != null && previousNode !== this) {
-            $(previousNode).css("fill", "#3e4153");
-            $(previousNode).next("text").css({
-                "fill": "white",
-                "font-weight": "normal"
-            });
-        }
-
-        isPlayingForward ? currentNodeIndex++ : currentNodeIndex--;
-
-        var nodeName = $("#node-" + node.id).data("node-name");
-
-        $(".data-heading-container span").html(nodeName);
-
-        var tableEntries = "";
-        $.each(node.table, function (index, tableEntry) {
-            tableEntries += "<tr><td>" + tableEntry.scope + "</td><td>" + tableEntry.id + "</td><td>" + tableEntry.type_address + "</td></tr>";
-        });
-        tableBody.html(tableEntries);
-        tableWrapper.scrollTop(tableWrapper.prop("scrollHeight"));
-
-        var explanations = "";
-        $.each(node.explanations, function (index, explanation) {
-            explanations += "<li>> " + explanation + "</li>";
-        });
-        explanationsText.html(explanations);
-        explanationsText.scrollTop(explanationsText.prop("scrollHeight"));
-
-        if (showGenerationAnimation) {
-            var objectCodeInstructions = "";
-            $.each(node.objectCode, function (index, objectCode) {
-                objectCodeInstructions += "<li>" + objectCode + "</li>";
-            });
-            objectCodeText.html(objectCodeInstructions);
-            var codeTemplateInstructions = "";
-            var codeTemplate = CodeTemplates.getTemplate(nodeName);
-            $.each(codeTemplate, function (index, codeTemplateInstruction) {
-                codeTemplateInstructions += "<li>> " + codeTemplateInstruction + "</li>";
-            });
-            codeTemplateText.html(codeTemplateInstructions);
-        }
-    }).on("end", function () {
-        previousNode = this;
-        if (hasAnimationFinished() && is_playing) {
-            is_playing = false;
-            enablePlayButton();
-        }
-    });
-}
-
-// Animates each node sequentially
-function animateTree() {
-    for (var i = currentNodeIndex, j = 0; i < nodeOrder.length - 1; i++, j++) {
-        var node = nodeOrder[i + 1];
-        animateNode(node, true, j);
-    }
-}
-
-// Start playing the animation, restart if already at the end
-function play() {
-    is_playing = true;
-    enablePauseButton();
-    if (hasAnimationFinished()) currentNodeIndex = -1;
-    animateTree();
-}
-
-// Pause the animation, interrupt all current animations
-function pause() {
-    is_playing = false;
-    enablePlayButton();
-    d3.selectAll("rect").interrupt();
-}
-
-// Move one node forward
-function forward() {
-    if (is_playing) pause();
-    if (!hasAnimationFinished()) {
-        var node = nodeOrder[currentNodeIndex + 1];
-        animateNode(node, true, 0);
-    }
-}
-
-// Move one node backward
-function reverse() {
-    if (is_playing) pause();
-    if (hasAnimationStarted()) {
-        var node = nodeOrder[currentNodeIndex - 1];
-        animateNode(node, false, 0);
-    }
-}
-
-// Show the play button, hide the pause button
-function enablePlayButton() {
-    $("#play-button").show();
-    $("#pause-button").hide();
-}
-
-// Show the pause button, hide the play button
-function enablePauseButton() {
-    $("#play-button").hide();
-    $("#pause-button").show();
-}
-
-// Check if the animation has reached the last node
-function hasAnimationFinished() {
-    return currentNodeIndex === nodeOrder.length - 1;
-}
-
-// Check if the animation has moved beyond the first node
-function hasAnimationStarted() {
-    return currentNodeIndex > 0;
-}
-
-/***/ }),
-/* 490 */
-/***/ (function(module, exports) {
-
 /* ==========================================================================
  * codeHelpers.js
  *
@@ -46038,6 +46013,18 @@ var CodeHelpers = module.exports = {
         return dataMap;
     }
 };
+
+/***/ }),
+/* 476 */
+/***/ (function(module, exports) {
+
+// removed by extract-text-webpack-plugin
+
+/***/ }),
+/* 477 */
+/***/ (function(module, exports) {
+
+// removed by extract-text-webpack-plugin
 
 /***/ })
 /******/ ]);
