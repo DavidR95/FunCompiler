b481d0a9b5cf0f2f7a4d8bc24575590a815a7a3a
David Robertson <dmichaelrobertson@gmail.com>
2017-12-23 11:32:01 +0000

    Extract code examples logic.

--- app/public/js/app.js
+++ app/public/js/app.js
@@ -45824,11 +45824,12 @@ var templates = {
 
 // Import CodeMirror node module
 var CodeMirror = __webpack_require__(48);
-
 // Import the CodeMirror 'simple-mode' addon
 __webpack_require__(178);
 // Import the CodeMirror 'active-line' addon
 __webpack_require__(179);
+// Import the CodeExamples module
+var CodeExamples = __webpack_require__(489);
 
 // Define a 'mode' for the Fun programming language, i.e., syntax highlighting
 CodeMirror.defineSimpleMode("fun", {
@@ -45849,32 +45850,14 @@ var cm = CodeMirror.fromTextArea(document.getElementById("code-editor"), {
 // Set the default value of the code mirror to be the program below
 cm.setValue("int n = 15\nproc main():\n\twhile n > 1:\n\t\tn = n/2\n\t.\n.");
 
-// Returns an example program given the example program name
-function getExample(exampleName) {
-    return examples[exampleName];
-}
-
 // Bind a 'click' event listener to all links with the class 'code-example'
 $(".code-example").on("click", function () {
     // Get the example name from the 'example' data-attribute and update
-    cm.setValue(getExample($(this).data("example")));
+    cm.setValue(CodeExamples.getExample($(this).data("example")));
 });
 
-// A list of example Fun programs
-var examples = {
-    ASSIGN: "proc main():\n\tint g = 7\n\tg = g + 1\n\tg = 1 + 2 * g\n\tg = (1 + 2) * g\n\twrite(g)\n.",
-    FACTORIAL: "func int fac(int n):\n\tint f = 1\n\twhile n > 1:\n\t\tf = f * n\n\t\tn = n - 1\n\t.\n\treturn f\n.",
-    FUNCTION: "func int test(int n):\n\tint r = 10\n\tint s = 20\n\tint t = 30\n\twrite(s)\n\treturn r\n.\n\nproc main():\n\twrite(test(5))\n.",
-    IF: "proc main():\n\tint m = 7\n\tint n = 3\n\tif m < n:\n\t\tm = m + 1\n\t\twrite(m)\n\telse:\n\t\tn = n + 1\n\t\twrite(n)\n\t.\n.",
-    IO: "int p = read()\n\nproc main():\n\tint q = read()\n\twrite(p)\n\twrite(q + 2/5)\n.",
-    OCTAL: "proc writeoctal(int n):\n\tif n < 8:\n\t\twrite(n)\n\telse:\n\t\twriteoctal(n/8)\n\twrite(n-((n/8)*8))\n\t.\n.",
-    PROC: "int total = 0\n\nproc add(int inc):\n\ttotal = total + inc\n.\n\nproc main():\n\tint i = read()\n\twhile i > 0:\n\t\tadd(i)\n\t\ti = read()\n\t.\n\twrite(total)\n.",
-    SCOPE_CHECKING: "int y = x # Error\nbool x = true # Error\n\nproc main():\n\tint n = 0\n\tint x = 0\n\tint n = 1 #Error\n\tx = x + y #Error\n\tp() #Error\n.",
-    TYPE_CHECKING: "int n = true # Error\nbool c = 1\n\nfunc bool pos(int n):\n\treturn n # Error\n.\n\nproc main():\n\tint i = 3\n\tbool b = true\n\ti = i + 1\n\ti = b # Error\n\ti = b * 2 # Error\n\tb = i > 0\n\tif b: write(i) .\n\tif i: write(i) . # Error\n\tb = pos(true) # Error\n\twhile pos(7):\n\t\ti = i + 1\n\t.\n.",
-    WHILE: "proc main():\n\tint m = read()\n\tint n = 1\n\twhile n * n < m + 1:\n\t\twrite(n * n)\n\t\tn = n + 1\n\t.\n."
-
-    // Below are readonly code snippets used within the specification
-};var overview_cm = CodeMirror(document.getElementById("overview").getElementsByClassName("code-snippet")[0], {
+// Below are readonly code snippets used within the specification
+var overview_cm = CodeMirror(document.getElementById("overview").getElementsByClassName("code-snippet")[0], {
     lineNumbers: true,
     tabSize: 2,
     lineWrapping: true,
@@ -45898,6 +45881,29 @@ $('.nav-tabs a').on('shown.bs.tab', function () {
     overview_cm.refresh();
     predefined_cm.refresh();
 });
+
+/***/ }),
+/* 489 */
+/***/ (function(module, exports) {
+
+var CodeExamples = module.exports = {
+    getExample: function getExample(exampleName) {
+        return examples[exampleName];
+    }
+};
+
+var examples = {
+    ASSIGN: "proc main():\n\tint g = 7\n\tg = g + 1\n\tg = 1 + 2 * g\n\tg = (1 + 2) * g\n\twrite(g)\n.",
+    FACTORIAL: "func int fac(int n):\n\tint f = 1\n\twhile n > 1:\n\t\tf = f * n\n\t\tn = n - 1\n\t.\n\treturn f\n.",
+    FUNCTION: "func int test(int n):\n\tint r = 10\n\tint s = 20\n\tint t = 30\n\twrite(s)\n\treturn r\n.\n\nproc main():\n\twrite(test(5))\n.",
+    IF: "proc main():\n\tint m = 7\n\tint n = 3\n\tif m < n:\n\t\tm = m + 1\n\t\twrite(m)\n\telse:\n\t\tn = n + 1\n\t\twrite(n)\n\t.\n.",
+    IO: "int p = read()\n\nproc main():\n\tint q = read()\n\twrite(p)\n\twrite(q + 2/5)\n.",
+    OCTAL: "proc writeoctal(int n):\n\tif n < 8:\n\t\twrite(n)\n\telse:\n\t\twriteoctal(n/8)\n\twrite(n-((n/8)*8))\n\t.\n.",
+    PROC: "int total = 0\n\nproc add(int inc):\n\ttotal = total + inc\n.\n\nproc main():\n\tint i = read()\n\twhile i > 0:\n\t\tadd(i)\n\t\ti = read()\n\t.\n\twrite(total)\n.",
+    SCOPE_CHECKING: "int y = x # Error\nbool x = true # Error\n\nproc main():\n\tint n = 0\n\tint x = 0\n\tint n = 1 #Error\n\tx = x + y #Error\n\tp() #Error\n.",
+    TYPE_CHECKING: "int n = true # Error\nbool c = 1\n\nfunc bool pos(int n):\n\treturn n # Error\n.\n\nproc main():\n\tint i = 3\n\tbool b = true\n\ti = i + 1\n\ti = b # Error\n\ti = b * 2 # Error\n\tb = i > 0\n\tif b: write(i) .\n\tif i: write(i) . # Error\n\tb = pos(true) # Error\n\twhile pos(7):\n\t\ti = i + 1\n\t.\n.",
+    WHILE: "proc main():\n\tint m = read()\n\tint n = 1\n\twhile n * n < m + 1:\n\t\twrite(n * n)\n\t\tn = n + 1\n\t.\n."
+};
 
 /***/ })
 /******/ ]);
--- app/resources/assets/js/codeEditors.js
+++ app/resources/assets/js/codeEditors.js
@@ -10,11 +10,12 @@
 
 // Import CodeMirror node module
 var CodeMirror = require('codemirror');
-
 // Import the CodeMirror 'simple-mode' addon
 require('codemirror/addon/mode/simple.js');
 // Import the CodeMirror 'active-line' addon
 require('codemirror/addon/selection/active-line.js');
+// Import the CodeExamples module
+var CodeExamples = require('./codeExamples.js')
 
 // Define a 'mode' for the Fun programming language, i.e., syntax highlighting
 CodeMirror.defineSimpleMode("fun", {
@@ -45,30 +46,11 @@ var cm = CodeMirror.fromTextArea(document.getElementById("code-editor"), {
 // Set the default value of the code mirror to be the program below
 cm.setValue("int n = 15\nproc main():\n\twhile n > 1:\n\t\tn = n/2\n\t.\n.");
 
-// Returns an example program given the example program name
-function getExample(exampleName) {
-    return examples[exampleName];
-}
-
 // Bind a 'click' event listener to all links with the class 'code-example'
 $(".code-example").on("click", function() {
     // Get the example name from the 'example' data-attribute and update
-    cm.setValue(getExample($(this).data("example")));
+    cm.setValue(CodeExamples.getExample($(this).data("example")));
 });
-
-// A list of example Fun programs
-var examples = {
-    ASSIGN: "proc main():\n\tint g = 7\n\tg = g + 1\n\tg = 1 + 2 * g\n\tg = (1 + 2) * g\n\twrite(g)\n.",
-    FACTORIAL: "func int fac(int n):\n\tint f = 1\n\twhile n > 1:\n\t\tf = f * n\n\t\tn = n - 1\n\t.\n\treturn f\n.",
-    FUNCTION: "func int test(int n):\n\tint r = 10\n\tint s = 20\n\tint t = 30\n\twrite(s)\n\treturn r\n.\n\nproc main():\n\twrite(test(5))\n.",
-    IF: "proc main():\n\tint m = 7\n\tint n = 3\n\tif m < n:\n\t\tm = m + 1\n\t\twrite(m)\n\telse:\n\t\tn = n + 1\n\t\twrite(n)\n\t.\n.",
-    IO: "int p = read()\n\nproc main():\n\tint q = read()\n\twrite(p)\n\twrite(q + 2/5)\n.",
-    OCTAL: "proc writeoctal(int n):\n\tif n < 8:\n\t\twrite(n)\n\telse:\n\t\twriteoctal(n/8)\n\twrite(n-((n/8)*8))\n\t.\n.",
-    PROC: "int total = 0\n\nproc add(int inc):\n\ttotal = total + inc\n.\n\nproc main():\n\tint i = read()\n\twhile i > 0:\n\t\tadd(i)\n\t\ti = read()\n\t.\n\twrite(total)\n.",
-    SCOPE_CHECKING: "int y = x # Error\nbool x = true # Error\n\nproc main():\n\tint n = 0\n\tint x = 0\n\tint n = 1 #Error\n\tx = x + y #Error\n\tp() #Error\n.",
-    TYPE_CHECKING: "int n = true # Error\nbool c = 1\n\nfunc bool pos(int n):\n\treturn n # Error\n.\n\nproc main():\n\tint i = 3\n\tbool b = true\n\ti = i + 1\n\ti = b # Error\n\ti = b * 2 # Error\n\tb = i > 0\n\tif b: write(i) .\n\tif i: write(i) . # Error\n\tb = pos(true) # Error\n\twhile pos(7):\n\t\ti = i + 1\n\t.\n.",
-    WHILE: "proc main():\n\tint m = read()\n\tint n = 1\n\twhile n * n < m + 1:\n\t\twrite(n * n)\n\t\tn = n + 1\n\t.\n."
-}
 
 // Below are readonly code snippets used within the specification
 var overview_cm = CodeMirror(document.getElementById("overview").getElementsByClassName("code-snippet")[0], {
